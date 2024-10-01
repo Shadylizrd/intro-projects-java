ae3d9fbe51357d22615742817046b34bc89bb6e9
Shadylizrd <rhiannonsummers88@gmail.com>
2024-10-01 17:04:21 +0100

    Practicing different methods of outputting lines

---
+++ HideAndSeek.java
@@ -0,0 +1,12 @@
+// Seems you must declare the first class to have the same name as the file
+public class HideAndSeek {
+    public static void main(String[] args) {
+// When using System.out.print everything is outputted on the same line!
+// println stands for "print line"
+        System.out.println("Let's play hide and seek. ");
+        System.out.print("Three...");
+        System.out.print("Two...");
+        System.out.println("One...");
+        System.out.println("Ready or not, here I come!");
+    }
+}
