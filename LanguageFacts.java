866b14911e36a956ef1c3778c9e1886751c85ddf
Shadylizrd <rhiannonsummers88@gmail.com>
2024-10-01 18:10:09 +0100

    Code Academy part 1

---
+++ LanguageFacts.java
@@ -0,0 +1,18 @@
+public class LanguageFacts {
+    public static void main(String[] args) {
+        // Press enter or return on your keyboard after each semicolon!
+        /*
+        Task on code academy detailiing how Java does not interpret
+        whitespace, you could have all the code here on the same line as long
+        as the semicolons are present. However this makes the code incredibly 
+        unreadable for other people who may view your code
+        */         
+        System.out.println("Java is a class-based language.");
+
+        System.out.println("Java classes have a 'main' method.");
+
+        System.out.println("Java statements end with a semicolon.");
+        
+        System.out.println("Programming is exciting!");
+      }
+}
