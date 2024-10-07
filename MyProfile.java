22c87358b3980718218af349f6841def295101a1
Shadylizrd <rhiannonsummers88@gmail.com>
2024-10-07 15:56:10 +0100

    Review task for part two of the course

---
+++ MyProfile.java
@@ -0,0 +1,12 @@
+// Final task for the second part of the java course
+
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Rhiannon";
+        int age = 18;
+        double desiredSalary = 999999999999.99;
+        char gender = 'f';
+        boolean lookingForJob = true;
+    }
+}
+// task was to use the correct data types for each variable
