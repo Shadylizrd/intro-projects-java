28c32240cecd0772a4a92c61a8d8088100177730
Shadylizrd <rhiannonsummers88@gmail.com>
2024-10-14 23:23:54 +0100

    Part 4: Classes and OOP

---
+++ Dog.java
@@ -0,0 +1,11 @@
+// Exercise expanding on how you create attributes for a class
+public class Dog{
+
+    public String name;
+    public String breed;
+    public int weight;
+  
+    public Dog(){
+      //DO NOT WRITE ANYTHING HERE!!
+    }
+  }
---
+++ Main.java
@@ -0,0 +1,7 @@
+// Exercise explaining how instances of classes can be created
+public class Main{
+    public static void main(String[] args){
+      Store newStore = new Store();
+    
+    }
+  }
